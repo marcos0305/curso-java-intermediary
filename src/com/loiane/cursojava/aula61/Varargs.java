19c6599aa16fadffbfc319777549b61719cc5b2a
marcos0305 <carvalho.oliveira0305@gmail.com>
2023-06-28 09:33:38 -0300

    Curso de Java 62: Varargs

---
+++ src/com/loiane/cursojava/aula61/Varargs.java
@@ -0,0 +1,32 @@
+package com.loiane.cursojava.aula61;
+
+public class Varargs {
+
+	public static void main(String[] args) {
+
+		int[] vetor = { 1, 2, 3, 4, 5 };
+		System.out.println(soma(vetor));
+
+		
+		System.out.println(soma(1, 2, 3, 4, 5, 6, 7, 8, 9));
+	}
+
+	static int soma(int[] vetor) {
+
+		int total = 0;
+
+		for (int i = 0; i < vetor.length; i++) {
+			total += vetor[i];
+		}
+		return total;
+	}
+
+	// VARARGS
+	static int soma(Integer... vetor) {
+		int total = 0;
+		for (int i=0; i< vetor.length; i++) {
+			total += vetor[i];
+		}
+		return total;
+	}
+}
