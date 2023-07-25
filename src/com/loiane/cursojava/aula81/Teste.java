657f302ba12939323bdee5a4bdfebcda56922366
marcos0305 <carvalho.oliveira0305@gmail.com>
2023-07-25 20:34:01 -0300

    Curso de Java #81: String: modificando letras maiúsculas/minúsculas (toLowerCase, toUpperCase)

---
+++ src/com/loiane/cursojava/aula81/Teste.java
@@ -0,0 +1,12 @@
+package com.loiane.cursojava.aula81;
+
+public class Teste {
+	
+	public static void main(String [] args) {
+		
+		String teste = "Teste";
+		
+		System.out.println(teste.toLowerCase());
+		System.out.println(teste.toUpperCase());
+	}
+}
