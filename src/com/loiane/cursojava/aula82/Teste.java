09ae890163c619e57df9ac57f4c53b607f38a7df
marcos0305 <carvalho.oliveira0305@gmail.com>
2023-07-26 10:00:50 -0300

    Curso de Java #83: concatenação com StringBuilder e StringBuffer

---
+++ src/com/loiane/cursojava/aula82/Teste.java
@@ -0,0 +1,33 @@
+package com.loiane.cursojava.aula82;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		String[] letras = {"A", "B", "C", "D"};
+		String alfabeto = "";
+		
+		for(String letra : letras) {
+			alfabeto += letra;						
+		}
+		System.out.println(alfabeto);
+		
+		//Metodo buffer
+		StringBuffer sb = new StringBuffer();
+		for(String letra: letras) {
+			sb.append(letra);
+		}
+		alfabeto = sb.toString();
+		System.out.println(alfabeto);
+		
+		
+		//Metodo Builder
+		StringBuilder dc = new StringBuilder() ;
+		for(String letra : letras) {
+			dc.append(letra);
+		}
+		alfabeto = dc.toString();
+		System.out.println(alfabeto);
+	}
+
+}
